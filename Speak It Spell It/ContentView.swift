f7833569720caa293cfcb134673bbc5056552be3
David Kramer <bacongravy@icloud.com>
2020-05-23 16:29:59 -0700

    Initial Commit

---
+++ Speak It Spell It/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Speak It Spell It
+//
+//  Created by David Kramer on 5/23/20.
+//  Copyright © 2020 David Kramer. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
