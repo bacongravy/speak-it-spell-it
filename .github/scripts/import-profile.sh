beac6687a912822695097f69f215493e3885ab67
David Kramer <davidkramer@me.com>
2020-06-12 23:12:58 -0700

    Move long steps to scripts

---
+++ .github/scripts/import-profile.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+set -euo pipefail
+
+mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
+echo "$PROVISIONING_PROFILE_DATA" | base64 --decode > ~/Library/MobileDevice/Provisioning\ Profiles/${PROVISIONING_PROFILE_UUID}.mobileprovision
